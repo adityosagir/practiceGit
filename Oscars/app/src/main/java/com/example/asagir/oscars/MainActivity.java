600841af4f3a7fcbf1c083242ce2e2bbf6d0f91c
adityosagir <adityo.sagir@gmail.com>
2016-02-29 12:51:41 -0500

    trying to mess up merge

--- Oscars/app/src/main/java/com/example/asagir/oscars/MainActivity.java
+++ Oscars/app/src/main/java/com/example/asagir/oscars/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.asagir.oscars;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView listView = (ListView) findViewById(R.id.listView);
+
+        // get the data from the adapter
+
+        ListAdapter actorAdapter = new ListAdapter(MainActivity.this, android.R.layout.simple_list_item_1, i forgot what goes here );
+
+        listView.setAdapter(actorAdapter);
     }
 }
